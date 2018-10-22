db96d85ca70c64ecd2e794630f884fdac4854edd
Daniel Pawłowski <eldan3k@gmail.com>
2018-10-22 18:29:02 +0200

    Interfaces fun

--- src/others/exceptions/Exceptions.java
+++ src/others/exceptions/Exceptions.java
@@ -9,9 +9,9 @@ public class Exceptions {
             throw new Exception("Hello ");
         } catch (Exception e) {
             System.out.print(e.getMessage());
-            // already caught, throws compile error
-//        } catch (IOException e) {
-//            System.out.print(e.getMessage());
+/*            // already caught, throws compile error - always need to order exceptions from most specialized to most broad ones
+        } catch (IOException e) {
+            System.out.print(e.getMessage());*/
         } finally {
             System.out.println("World");
         }
